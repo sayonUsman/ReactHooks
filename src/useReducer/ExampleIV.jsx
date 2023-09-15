41363c65ce3a4ea18e5417de075a6a62092ec06b
Sayon Usman <sayonusman.ma@gmail.com>
2023-09-15 18:58:00 +0300

    Added example-IV of useReducer hook

---
+++ src/useReducer/ExampleIV.jsx
@@ -0,0 +1,65 @@
+import { useReducer } from "react";
+import "../style.css";
+
+const ExampleIV = () => {
+  const initialState = -1;
+  const initialState2 = 0;
+
+  const reducer = (currentState, action) => {
+    switch (action) {
+      case "increment":
+        return currentState + 1;
+      case "decrement":
+        return currentState - 1;
+      default:
+        return currentState;
+    }
+  };
+
+  const [count, dispatch] = useReducer(reducer, initialState);
+  const [count2, dispatch2] = useReducer(reducer, initialState2);
+
+  return (
+    <>
+      <h1>useReducer</h1>
+
+      <div>
+        <h2> Count I = {count}</h2>
+
+        <button
+          style={{ marginRight: "15px" }}
+          type="button"
+          onClick={() => dispatch("increment")}
+        >
+          Increment
+        </button>
+
+        <button
+          style={{ marginRight: "15px" }}
+          type="button"
+          onClick={() => dispatch("decrement")}
+        >
+          Decrement
+        </button>
+      </div>
+
+      <div>
+        <h2> Count II = {count2}</h2>
+
+        <button
+          style={{ marginRight: "15px" }}
+          type="button"
+          onClick={() => dispatch2("increment")}
+        >
+          Increment
+        </button>
+
+        <button type="button" onClick={() => dispatch2("decrement")}>
+          Decrement
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default ExampleIV;
